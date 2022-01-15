f29677acef50a3c1318789680e02427cf310202b
Carlos Queiroz <carlostgti@outlook.com>
2022-01-14 23:09:15 -0300

    adjusting home spec

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe('home page', () => {
   it('app deve estar online', () => {
-    cy.viewport(1440, 900)
-    cy.visit('https://buger-eats.vercel.app/');
+    cy.visit('/');
     cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
   })
 });
